6d32e7efbef88255639e0e0b2381fbe02b361ac4
bovey <paul.player@gmail.com>
2020-02-10 15:02:39 +0000

    Puzzles Complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,16 +21,24 @@ public class Player : MonoBehaviour
     private bool _canDoubleJump = false;
     private Vector3 _direction;
     private Vector3 _velocity;
+    private Vector3 _wallJumpDirection;
     private float _yVelocity;
+    private bool _canWalljump = false;
+    private float _pushPower = 2.0f;
+
 
     void Start()
     {
         _controller = GetComponent<CharacterController>();
-        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        if (_controller == null)
+        {
+            Debug.LogError("Player: Controller is NULL.");
+        }
 
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
         if (_uiManager == null)
         {
-            Debug.LogError("The UI Manager is NULL."); 
+            Debug.LogError("Player : UIManager is NULL."); 
         }
 
         _uiManager.UpdateLivesDisplay(_lives);
@@ -45,9 +53,12 @@ public class Player : MonoBehaviour
     {
         if (_controller.isGrounded == true)
         {
+            _wallJumpDirection = Vector3.zero;
+            _canWalljump = false;
             float horizontalInput = Input.GetAxis("Horizontal");
             _direction = new Vector3(horizontalInput, 0, 0);
             _velocity = _direction * _speed;
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _yVelocity = _jumpHeight;
@@ -58,10 +69,16 @@ public class Player : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (_canDoubleJump == true)
+                if (_canDoubleJump)
                 {
                     _yVelocity += _jumpHeight;
                     _canDoubleJump = false;
+                }
+                
+                if (_canWalljump)
+                {
+                    _yVelocity += _jumpHeight;
+                    _velocity = _wallJumpDirection * _speed;
                 }
             }
 
@@ -73,6 +90,32 @@ public class Player : MonoBehaviour
         _controller.Move(_velocity * Time.deltaTime);
     }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "Wall" && _controller.isGrounded == false)
+        {
+            Debug.DrawRay(hit.point, hit.normal, Color.blue);
+            _canWalljump = true;
+            _wallJumpDirection = hit.normal;
+        }
+
+        if (hit.transform.tag == "Moveable")
+        {
+            // make sure the hit object has a rigidbody
+            Rigidbody objBody = hit.collider.attachedRigidbody;
+            if (objBody == null || objBody.isKinematic)
+            {
+                //no rigidbody or not able to affect it
+                return;
+            }
+
+            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, 0);
+            objBody.velocity = pushDir * _pushPower;
+        }
+
+    }
+
+    
     public void AddCoins()
     {
         _coins++;
---
+++ Assets/Scripts/PressurePad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer _padDisplay;
+    [SerializeField]
+    private float _hitRange = 0.1f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Moveable")
+        {
+            if ((other.transform.position.x >= this.transform.position.x - _hitRange) &&
+                    (other.transform.position.x <= this.transform.position.x + _hitRange))
+            {
+                Rigidbody movingObject = other.attachedRigidbody;
+                if (movingObject != null)
+                {
+                    movingObject.isKinematic = true;
+                }
+
+                _padDisplay.material.color = Color.green;
+                Destroy(this);
+            }
+        }
+    }
+}
